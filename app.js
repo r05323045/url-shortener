23fdae893428072826d783e9acc8f22064b1c991
r05323045 <r05323045@ntu.edu.tw>
2020-10-10 23:15:17 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 
-// 設定首頁路由
+mongoose.connect('mongodb://localhost/url-shortener', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-// 設定 port 3000
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
