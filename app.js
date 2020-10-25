bc0cabe82f40ca71f21915b3c63449f0fae96b38
r05323045 <r05323045@ntu.edu.tw>
2020-10-26 00:24:07 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const urlShortener = require('./url_shortener')
 const Shortener = require('./models/shortener')
+const routes = require('./routes')
 
 const app = express()
 
@@ -23,45 +23,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
-
-app.get('/', (req, res) => {
-  Shortener.find()
-    .lean()
-    .then(urls => res.render('index', { urls }))
-    .catch(error => console.error(error))
-})
-
-app.post('/', (req, res) => {
-  const input_url = req.body.url
-  return Shortener.find()
-    .lean()
-    .then(urls => {
-      let output_url = urlShortener()
-      urls.forEach(url => {
-        const output = url.output_url.slice(url.output_url.length - 5, url.output_url.length)
-        if (output_url === output) {
-          output_url = urlShortener()
-        }
-      })
-      Shortener.create({input_url: input_url, output_url: output_url})
-      res.render('result', { output_url: 'http://localhost:3000/' + output_url})
-    })
-    .catch(error => console.log(error))
-})
-
-app.get('/:output_url', (req, res) => {
-  const output_url = req.params.output_url
-  Shortener.find()
-    .lean()
-    .then(urls => 
-      urls.forEach(url => {
-        if (url.output_url === output_url) {
-          res.redirect(url.input_url)
-        }
-      })
-    )
-    .catch(error => console.error(error))
-})
+app.use(routes)
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
---
+++ routes/modules/home.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const router = express.Router()
+
+const urlShortener = require('../../url_shortener')
+const Shortener = require('../../models/shortener')
+
+router.get('/', (req, res) => {
+  Shortener.find()
+    .lean()
+    .then(urls => res.render('index', { urls }))
+    .catch(error => console.error(error))
+})
+
+router.post('/', (req, res) => {
+  const inputUrl = req.body.url
+  return Shortener.find()
+    .lean()
+    .then(urls => {
+      let outputUrl = urlShortener()
+      urls.forEach(url => {
+        const output = url.output_url.slice(url.output_url.length - 5, url.output_url.length)
+        if (outputUrl === output) {
+          outputUrl = urlShortener()
+        }
+      })
+      Shortener.create({ input_url: inputUrl, output_url: outputUrl })
+      res.render('result', { output_url: 'http://localhost:3000/' + outputUrl })
+    })
+    .catch(error => console.log(error))
+})
+
+router.get('/:output_url', (req, res) => {
+  const outputUrl = req.params.output_url
+  Shortener.find()
+    .lean()
+    .then(urls =>
+      urls.forEach(url => {
+        if (url.output_url === outputUrl) {
+          console.log(url.input_url)
+          res.redirect(url.input_url)
+        }
+      })
+    )
+    .catch(error => console.error(error))
+})
+
+module.exports = router
