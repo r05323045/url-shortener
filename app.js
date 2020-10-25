791f24aa4d41d9e4d4a1a9eefa0894dc25fc9a26
r05323045 <r05323045@ntu.edu.tw>
2020-10-25 10:17:10 +0800

    feat: create result.hbs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Shortener = require('./models/shortener')
 
 const app = express()
@@ -20,11 +21,27 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   Shortener.find()
     .lean()
     .then(urls => res.render('index', { urls }))
     .catch(error => console.error(error))
+})
+
+app.post('/', (req, res) => {
+  const url = req.body
+  return Shortener.find()
+    .lean()
+    .then(urls => {
+      urls.forEach(url => {
+        console.log(url.output_url)
+      })
+      const output_url = "https://www.google.com.tw/"
+      res.render('result', { output_url })
+    })
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
