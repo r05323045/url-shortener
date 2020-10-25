2a7d7b0c5c8b08c65c71a1a57d73a41a38a6a72f
r05323045 <r05323045@ntu.edu.tw>
2020-10-26 00:47:18 +0800

    modify: shortenerSeeder.js

--- models/seeds/shortenerSeeder.js
+++ models/seeds/shortenerSeeder.js
@@ -4,9 +4,9 @@ const db = require('../../config/mongoose')
 db.once('open', () => {
   console.log('mongodb connected!')
   Shortener.create(
-    { input_url: 'https://www.google.com.tw/', output_url: 'http://localhost:3000/6y7UP' },
-    { input_url: 'https://www.facebook.com/', output_url: 'http://localhost:3000/jcmB3' },
-    { input_url: 'https://developer.mozilla.org/en-US/', output_url: 'http://localhost:3000/Ecp3Y' }
+    { input_url: 'https://www.google.com.tw/', output_url: '6y7UP' },
+    { input_url: 'https://www.facebook.com/', output_url: 'jcmB3' },
+    { input_url: 'https://developer.mozilla.org/en-US/', output_url: 'Ecp3Y' }
   )
   console.log('done')
 })
