88aaa3c5cbd193693d2d630d51d2b852e84f0af6
r05323045 <r05323045@ntu.edu.tw>
2020-10-26 00:28:39 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -2,22 +2,10 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const Shortener = require('./models/shortener')
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
-
-mongoose.connect('mongodb://localhost/url-shortener', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
--- models/seeds/shortenerSeeder.js
+++ models/seeds/shortenerSeeder.js
@@ -1,13 +1,6 @@
-const mongoose = require('mongoose')
 const Shortener = require('../shortener')
-mongoose.connect('mongodb://localhost/url-shortener', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+const db = require('../../config/mongoose')
+
 db.once('open', () => {
   console.log('mongodb connected!')
   Shortener.create(
