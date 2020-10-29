50b2acedb6276dc41a81a0d7a4d32ef657ff8986
r05323045 <r05323045@ntu.edu.tw>
2020-10-29 23:50:03 +0800

    revised url_shortener.js

--- url_shortener.js
+++ url_shortener.js
@@ -1,16 +1,24 @@
 function randomNum () {
-  const num = 48 + Math.ceil((Math.random() * 74))
-  if ((num > 90 && num < 97) || (num > 57 && num < 65)) {
-    return randomNum()
-  } else {
-    return num
+  let num = '0'.charCodeAt() + Math.floor((Math.random() * ('z'.charCodeAt() - '0'.charCodeAt())))
+  while ((num > 'Z'.charCodeAt() && num < 'a'.charCodeAt()) || (num > '9'.charCodeAt() && num < 'A'.charCodeAt())) {
+    num = '0'.charCodeAt() + Math.floor((Math.random() * ('z'.charCodeAt() - '0'.charCodeAt())))
   }
+  return num
 }
 
 function randomWords (len) {
+  let nums = []
+  const isAllNum = (num) => (num <= '9'.charCodeAt())
+  const isAllLetter = (num) => (num > '9'.charCodeAt())
+  while (nums.every(isAllNum) || nums.every(isAllLetter)) {
+    nums = []
+    for (let i = 0; i < len; i++) {
+      nums.push(randomNum())
+    }
+  }
   let words = ''
   for (let i = 0; i < len; i++) {
-    words += String.fromCharCode(randomNum())
+    words += String.fromCharCode(nums[i])
   }
   return words
 }
